dc2d20af1cf0d435759354d78d2e0bc782d57605
vitinmottin <166723677+vitinmottin@users.noreply.github.com>
2024-10-04 09:14:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaAlternativa = document.querySelector('.caixa-alternativa');
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const perguntas = [//abre a lista de objetos (itens)
+    {//abre o item
+        enunciado: "Você gosta da Inteligência Artificial?",
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "Afirmação da alternativa 1"
+        },
+        {
+            texto: "Não",
+            afirmação: "Afirmação da alternativa"
+        }
+        ]
+    },
+    {
+        enunciado: "Você acha uma ameaça a Inteligência Artificial?",
+        alternativas: [{
+            texto: "Quem sabe...",
+            afirmação: "Afirmação da alternativa 1"
+        },
+        {
+            texto:"Talvez",
+            afirmação:"Afirmação da alternativa 2"
+        }
+        ]
+    },
+    {
+        enunciado: "",
+        alternativas: [{
+            texto: "Sim",
+            afirmação: "Afirmação da alternativa 1"
+        },
+        {
+            texto: "Não",
+            afirmação: "Afirmação da alternativa"
+        }
+        ]
+    }
+]
+let posicao = 0;
+let perguntaAtual;
+let resposta = "";
+
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[posicao];
+    caixaPergunta.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click"() =>respostasSelecionadas(alternativas));
+        caixaAlternativa.appendChild(botaoAlternativas);
+    }
+}
+function respostasSelecionadas(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmação;
+    respostas = afirmacoes;
+    posicao++;
+    mostraPergunta();
+}
+mostraPergunta();
